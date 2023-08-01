9d77f542e33307d9e7831ea0352cab4ba3d264cf
francesca <francipeco36@gmail.com>
2023-08-01 16:40:14 +0200

    main completed

--- src/lessons/java/agenzia/Main.java
+++ src/lessons/java/agenzia/Main.java
@@ -1,19 +1,49 @@
 package lessons.java.agenzia;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        // creo i miei immobili
         Box box1 = new Box("B001", "Viale Michelangelo, 150", "90145",
-                "Palermo", 45, 1, 3);
+                "Palermo", 45, 0, 3);
         Abitazione appartamento1 = new Abitazione("A001", "Via Leonardo Da Vinci, 238",
-                "90135", "Palermo", 95, 1, 3, 1);
+                "90135", "Palermo", 95, 0, 3, 1);
         Villa villa1 = new Villa("A002", "Via Francesco Baracca, 35", "90136",
-                "Palermo", 150,1, 5, 2, 20);
+                "Palermo", 150,0, 5, 2, 20);
+        AgenziaImmobiliare agenzia = new AgenziaImmobiliare();
 
-    AgenziaImmobiliare agenzia = new AgenziaImmobiliare();
-    agenzia.aggiungiImmobile(box1);
-    agenzia.aggiungiImmobile(appartamento1);
-    agenzia.aggiungiImmobile(villa1);
+        // testo metodo aggiungiImmobile
+        agenzia.aggiungiImmobile(box1);
+        agenzia.aggiungiImmobile(appartamento1);
+        agenzia.aggiungiImmobile(villa1);
+        System.out.println(agenzia);
+
+        // testo metodo reimpostaSuperfcie
+        appartamento1.reimpostaSuperficie(97);
+        System.out.println(appartamento1);
+        villa1.reimpostaSuperficie(147, 23);
+        System.out.println(villa1);
+
+        // testo metodo aggiungiInteressati
+        appartamento1.aggiungiInteressati();
+        System.out.println("Appartamento 1 - Interessati: " + appartamento1.getInteressati());
+
+        // testo metodo trovaImmobile
+        System.out.println("Sto cercando un immobile con codice A001... ");
+        System.out.println(agenzia.trovaImmobile("A001"));
+        System.out.println("Sto cercando un immobile con codice A006... ");
+        System.out.println(agenzia.trovaImmobile("A006"));
+
+        // testo metodo per vedere l'immobile con più interessati
+        System.out.println(agenzia.conPiuInteressati() + " è l'immobile con più interessati.");
+        // aumento gli interessati a un altro immobile per testare nuovamente
+        // il metodo conPiuInteressati
+        villa1.aggiungiInteressati();
+        villa1.aggiungiInteressati();
+        System.out.println(agenzia.conPiuInteressati() + " è l'immobile con più interessati.");
 
     }
 }
