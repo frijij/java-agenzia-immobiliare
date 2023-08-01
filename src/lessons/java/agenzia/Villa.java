22d57f8a2f0156677399bcdf1f2c8bcb299ddb8f
francesca <francipeco36@gmail.com>
2023-08-01 12:08:32 +0200

    Villa class made

--- src/lessons/java/agenzia/Villa.java
+++ src/lessons/java/agenzia/Villa.java
@@ -1,18 +1,37 @@
 package lessons.java.agenzia;
 
-public class Villa {
-    /*
-    Per le ville è previsto, in aggiunta rispetto all’abitazione, la dimensione in mq di giardino.
-     */
-
-    // ATTRIBUTI:
+public class Villa extends Abitazione{
+    // ATTRIBUTI: superficie del giardino
+    private int superficieGiardino;
 
     // COSTRUTTORI
+
+    public Villa(String codiceImmobile, String indirizzo, String cap, String citta, int superficieInMq, int interessati, int vani, int bagni, int superficieGiardino) {
+        super(codiceImmobile, indirizzo, cap, citta, superficieInMq, interessati, vani, bagni);
+        this.superficieGiardino = superficieGiardino;
+    }
 
     // GETTER E SETTER
 
     // METODI
-// Implementare nella classe Villa l’overload di questo metodo.
+// Implementare nella classe Villa l’overload di reimpostaSuperficie.
 // In questo caso prenderà i parametri superficie e superficieGiardino.
-
+    public void reimpostaSuperficie(int superficieInMq, int superficieGiardino) {
+        super.reimpostaSuperficie(superficieInMq);
+        this.superficieGiardino = superficieGiardino;
+    }
+    @Override
+    public String toString() {
+        return "Villa{" +
+                "codice immobile:'" + getCodiceImmobile() + '\'' +
+                ", indirizzo: '" + getIndirizzo() + '\'' +
+                ", CAP: '" + getCap() + '\'' +
+                ", città: '" + getCitta() + '\'' +
+                ", superficie in mq: " + getSuperficieInMq() +
+                ", vani: " + getVani() +
+                ", bagni: " + getBagni() +
+                ", superficie giardino: " + superficieGiardino +
+                ", interessati: " + getInteressati() +
+                '}';
+    }
 }
