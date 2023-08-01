de74e40060bb57627f803babf29758a7277423f8
francesca <francipeco36@gmail.com>
2023-08-01 11:35:57 +0200

    Immobile class made

--- src/lessons/java/agenzia/Immobile.java
+++ src/lessons/java/agenzia/Immobile.java
@@ -3,15 +3,71 @@ package lessons.java.agenzia;
 public class Immobile {
     // ATTRIBUTI: un codice alfanumerico, indirizzo, cap, città, una superficie espressa in mq attraverso un numero intero.,
     // interessati
+    private String codiceImmobile;
+    private String indirizzo;
+    private String cap;
+    private String citta;
+    private int superficieInMq;
+    private int interessati;
 
     // COSTRUTTORI
 
+    public Immobile(String codiceImmobile, String indirizzo, String cap, String citta, int superficieInMq, int interessati) {
+        this.codiceImmobile = codiceImmobile;
+        this.indirizzo = indirizzo;
+        this.cap = cap;
+        this.citta = citta;
+        this.superficieInMq = superficieInMq;
+        this.interessati = interessati;
+    }
+
+
     // GETTER E SETTER
+    public String getCodiceImmobile() {
+        return codiceImmobile;
+    }
+
+    public String getIndirizzo() {
+        return indirizzo;
+    }
+
+    public String getCap() {
+        return cap;
+    }
+
+    public String getCitta() {
+        return citta;
+    }
+
+    public int getSuperficieInMq() {
+        return superficieInMq;
+    }
+
+    public int getInteressati() {
+        return interessati;
+    }
 
     // METODI
     // incrementare il numero degli interessati
-/*
-Definire per ciascuna delle tre classi il metodo toString() in modo da fornire una descrizione completa dell’immobile.
-Definire nella classe Immobile il metodo reimpostaSuperfici che prende in input la nuova superficie.
- */
+    public int aggiungiInteressati(){
+        // TO DO
+        return interessati;
+    }
+
+    // reimpostare la superficie prendendo in input la nuova superficie
+    public void reimpostaSuperficie(int superficieInMq) {
+        this.superficieInMq = superficieInMq;
+    }
+
+    @Override
+    public String toString() {
+        return "Immobile{" +
+                "codice immobile:'" + codiceImmobile + '\'' +
+                ", indirizzo: '" + indirizzo + '\'' +
+                ", CAP: '" + cap + '\'' +
+                ", città: '" + citta + '\'' +
+                ", superficie in mq: " + superficieInMq +
+                ", interessati: " + interessati +
+                '}';
+    }
 }
