5d18c279f979534c29cb14a5fe3c7efab5adcc10
francesca <francipeco36@gmail.com>
2023-08-01 11:45:12 +0200

    Box class made

--- src/lessons/java/agenzia/Box.java
+++ src/lessons/java/agenzia/Box.java
@@ -1,11 +1,29 @@
 package lessons.java.agenzia;
 
-public class Box {
+public class Box extends Immobile{
     // ATTRIBUTI: numero di posti auto
+    private int numPostiAuto;
 
     // COSTRUTTORI
+    public Box(String codiceImmobile, String indirizzo, String cap, String citta, int superficieInMq, int interessati, int numPostiAuto) {
+        super(codiceImmobile, indirizzo, cap, citta, superficieInMq, interessati);
+        this.numPostiAuto = numPostiAuto;
+    }
 
     // GETTER E SETTER
 
     // METODI
+
+    @Override
+    public String toString() {
+        return "Box{" +
+                "codice immobile:'" + getCodiceImmobile() + '\'' +
+                ", indirizzo: '" + getIndirizzo() + '\'' +
+                ", CAP: '" + getCap() + '\'' +
+                ", città: '" + getCitta() + '\'' +
+                ", superficie in mq: " + getSuperficieInMq() +
+                ", posti auto: " + numPostiAuto +
+                ", interessati: " + getInteressati() +
+                '}';
+    }
 }
