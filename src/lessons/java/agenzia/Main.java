d6a32b3c6061b202ffaa330cf674ee7aa3235423
francesca <francipeco36@gmail.com>
2023-08-01 13:10:39 +0200

    working on main

--- src/lessons/java/agenzia/Main.java
+++ src/lessons/java/agenzia/Main.java
@@ -3,5 +3,17 @@ package lessons.java.agenzia;
 public class Main {
     public static void main(String[] args) {
 
+        Box box1 = new Box("B001", "Viale Michelangelo, 150", "90145",
+                "Palermo", 45, 1, 3);
+        Abitazione appartamento1 = new Abitazione("A001", "Via Leonardo Da Vinci, 238",
+                "90135", "Palermo", 95, 1, 3, 1);
+        Villa villa1 = new Villa("A002", "Via Francesco Baracca, 35", "90136",
+                "Palermo", 150,1, 5, 2, 20);
+
+    AgenziaImmobiliare agenzia = new AgenziaImmobiliare();
+    agenzia.aggiungiImmobile(box1);
+    agenzia.aggiungiImmobile(appartamento1);
+    agenzia.aggiungiImmobile(villa1);
+
     }
 }
